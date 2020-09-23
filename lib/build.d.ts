fd4939972ae532dfa95fa5f34d8d0f2e057f7015
Michel Denegri <micheldenegri@me.com>
2020-09-23 09:57:00 -0700

    dist for testing

---
+++ lib/build.d.ts
@@ -0,0 +1,9 @@
+import { FileFsRef, BuildOptions, Lambda, File } from '@vercel/build-utils';
+import { Route } from '@vercel/routing-utils';
+interface BuilderOutput {
+    watch?: string[];
+    output: Record<string, Lambda | File | FileFsRef>;
+    routes: Route[];
+}
+export declare function build({ files, entrypoint, workPath, config, meta }: BuildOptions): Promise<BuilderOutput>;
+export {};
---
+++ lib/utils.d.ts
@@ -0,0 +1,37 @@
+/// <reference types="node" />
+import { SpawnOptions } from 'child_process';
+import { ExecaReturnValue } from 'execa';
+import { Files, PackageJson } from '@vercel/build-utils';
+import { IOptions } from 'glob';
+import { NuxtConfig as NuxtConfiguration } from '@nuxt/types';
+declare type Mutable<T> = {
+    -readonly [P in keyof T]: T[P] extends ReadonlyArray<infer U> ? Mutable<U>[] : Mutable<T[P]>;
+};
+export declare type MutablePackageJson = Mutable<PackageJson>;
+export declare function exec(cmd: string, args: string[], { env, ...opts }?: SpawnOptions): Promise<ExecaReturnValue>;
+/**
+ * Read in a JSON file with support for UTF-16 fallback.
+ */
+export declare function readJSON<T = unknown>(filename: string): Promise<T>;
+/**
+ * Validate if the entrypoint is allowed to be used
+ */
+export declare function validateEntrypoint(entrypoint: string): void;
+export declare function renameFiles(files: Files, renameFn: (fileName: string) => string): Files;
+export declare function globAndRename(pattern: string, opts: IOptions | string, renameFn: (fileName: string) => string): Promise<Files>;
+export declare function globAndPrefix(pattern: string, opts: IOptions | string, prefix: string): Promise<Files>;
+interface NuxtVersion {
+    name: string;
+    version: string;
+    semver: string;
+    suffix: string;
+    section: string;
+}
+export declare function findNuxtDep(pkg: MutablePackageJson): void | NuxtVersion;
+export declare function preparePkgForProd(pkg: MutablePackageJson): NuxtVersion;
+export declare function hrToMs(hr: [number, number]): number;
+export declare function endStep(): void;
+export declare function startStep(step: string): void;
+export declare function getNuxtConfig(rootDir: string, nuxtConfigName: string): NuxtConfiguration;
+export declare function getNuxtConfigName(rootDir: string): string;
+export {};
